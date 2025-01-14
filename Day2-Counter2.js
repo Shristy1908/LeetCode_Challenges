62a3a58f1d0337cae982378e85d388e7253a8bcc
Shristy1908 <shristykumari1908@gmail.com>
2025-01-14 22:56:00 +0530

    counter-2 push

---
+++ Day2-Counter2.js
@@ -0,0 +1,32 @@
+//counter 2
+//https://leetcode.com/problems/counter-ii/description/?envType=study-plan-v2&envId=30-days-of-javascript
+// difficulty - Easy
+
+
+var createCounter = function (init) {
+  let presentVal = init;
+
+  function increment() {
+    presentVal++;
+    return presentVal;
+  }
+
+  function decrement() {
+    presentVal--;
+    return presentVal;
+  }
+
+  function reset() {
+    presentVal = init;
+    return presentVal;
+  }
+
+  return { increment, decrement, reset };
+};
+
+const counter = createCounter(5);
+let incrementedVal= counter.increment();
+let resetVal= counter.reset();
+let decrementedVal= counter.decrement();
+
+document.write(incrementedVal,resetVal,decrementedVal);
