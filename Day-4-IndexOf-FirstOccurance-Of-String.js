0895bb274262d271dec2756121820253cf0e3611
Shristy1908 <shristykumari1908@gmail.com>
2025-01-16 18:48:14 +0530

    index of first occurance of string push

---
+++ Day-4-IndexOf-FirstOccurance-Of-String.js
@@ -0,0 +1,10 @@
+var heystack = "sadshristysad";
+var needle = "Sad";
+
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+let result = strStr(heystack, needle);
+
+document.write(result);
