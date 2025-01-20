0a9c60611a81c306a3eefdcebce890cd9bef0deb
Shristy1908 <shristykumari1908@gmail.com>
2025-01-20 23:36:49 +0530

    missing number push

---
+++ Day-6-Missing-Number.js
@@ -0,0 +1,13 @@
+var nums = [0, 2, 4, 1];
+
+var missingNumber = function (nums) {
+  var numLen = nums.length;
+
+  var expectedSum = (numLen * (numLen + 1)) / 2;
+  var actualSum = nums.reduce((ac, sum) => ac + sum);
+
+  return expectedSum - actualSum;
+};
+
+var missingNo = missingNumber(nums);
+document.write(missingNo);
