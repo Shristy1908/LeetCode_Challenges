cd852e50b5465adc83b99a880aaa4649e2557745
Shristy1908 <shristykumari1908@gmail.com>
2025-02-05 23:32:45 +0530

    sqrt push

---
+++ Day-14-sqrt(x).js
@@ -0,0 +1,13 @@
+//sqrt(x)
+//https://leetcode.com/problems/sqrtx/description/
+//Difficulty - Easy
+
+var mySqrt = function (x) {
+  const sqrt = Math.sqrt(x);
+  return Math.floor(sqrt);
+};
+
+document.write(mySqrt(16));
+
+
+
